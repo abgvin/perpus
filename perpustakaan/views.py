78b2f1cebef270988f21db722c43662fb351ecb2
abgvin <muhamadapri10@gmail.com>
2020-11-18 17:11:22 +0700

    Template

--- perpustakaan/views.py
+++ perpustakaan/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def buku(request):
-    return HttpResponse('<h1>Halaman Buku</h1>')
+    return render(request, 'buku.html')
 
 def penulis(request):
     return HttpResponse('Halaman Penulis')
