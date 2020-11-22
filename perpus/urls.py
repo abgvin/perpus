fdef37b33d53188da3211315a7e9a747b7630ba9
abgvin <muhamadapri10@gmail.com>
2020-11-23 00:52:22 +0700

    crud mengubah data

--- perpus/urls.py
+++ perpus/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from perpustakaan.views import buku, penerbit, tambah_buku
+from perpustakaan.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', buku),
-    path('buku/', buku),
-    path('penerbit/', penerbit),
-    path('tambah-buku/', tambah_buku),
-
+    path('penerbit/', penerbit, name='penerbit'),
+    path('buku/', buku, name='buku'),
+    path('buku/tambah/', tambah_buku, name='tambah_buku'),
+    path('buku/ubah/<int:id_buku>', ubah_buku, name='ubah_buku'),
 ]
--- perpustakaan/views.py
+++ perpustakaan/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from perpustakaan.models import Buku
 from perpustakaan.form import FormBuku
+from django.contrib import messages
+
+def ubah_buku(request, id_buku):
+    buku = Buku.objects.get(id=id_buku)
+    template = 'ubah-buku.html'
+    page_title = 'Edit Buku'
+    if request.POST:
+        form = FormBuku(request.POST, request.FILES, instance=buku)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Data Berhasil diperbaharui!")
+            return redirect('ubah_buku', id_buku=id_buku)
+    else:
+        form = FormBuku(instance=buku)
+        konteks = {
+            'form':form,
+            'buku':buku,
+            'page_title':page_title,
+        }
+    return render(request, template, konteks)
+
 
 def buku(request):
     page_title = 'Books'
     books = Buku.objects.all()
-    # ? books = Buku.objects.filter(kelompok__nama='Produktif')[:2]
 
     datas = {
         'page_title': page_title,
