a6008eddc78da352816434527ec55122f8d6c135
abgvin <muhamadapri10@gmail.com>
2020-12-02 12:09:55 +0700

    Tambah user

--- perpus/urls.py
+++ perpus/urls.py
@@ -4,12 +4,14 @@ from perpustakaan.views import *
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path('', buku, name='buku'),
     path('admin/', admin.site.urls),
     path('penerbit/', penerbit, name='penerbit'),
     path('buku/', buku, name='buku'),
     path('buku/tambah/', tambah_buku, name='tambah_buku'),
     path('buku/ubah/<int:id_buku>', ubah_buku, name='ubah_buku'),
     path('buku/hapus/<int:id_buku>', hapus_buku, name='hapus_buku'),
-    path('masuk', LoginView.as_view(), name='masuk'),
-    path('keluar', LogoutView.as_view(next_page='masuk'), name='keluar'),
+    path('masuk/', LoginView.as_view(), name='masuk'),
+    path('keluar/', LogoutView.as_view(next_page='masuk'), name='keluar'),
+    path('signup/', signup, name='signup'),
 ]
--- perpustakaan/views.py
+++ perpustakaan/views.py
@@ -4,6 +4,7 @@ from perpustakaan.form import FormBuku
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
 
 @login_required(login_url=settings.LOGIN_URL)
 def ubah_buku(request, id_buku):
@@ -79,3 +80,23 @@ def hapus_buku(request, id_buku):
 
     messages.success(request, "Data berhasil dihapus")
     return redirect('buku')
+
+@login_required(login_url=settings.LOGIN_URL)
+def signup(request):
+    if request.POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "User berhasil dibuat")
+            return redirect('signup')
+        else:
+            messages.error(request, "Terjadi kesalahan!")
+            return redirect('signup')
+    else:
+        form = UserCreationForm()
+        konteks = {
+            'form': form,
+        }
+    
+    return render(request, 'signup.html', konteks)
+
